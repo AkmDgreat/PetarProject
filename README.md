1bc5d188c040f7cb933daea6d17b513362b56547
Akshat Mishra <admiralakshat@gmail.com>
2024-04-30 18:29:59 -0700

    added README

---
+++ README.md
@@ -0,0 +1,4 @@
+install `win32ui` using `pip install pywin32`, and import it using `import win32ui`
+
+install `Spark` from https://sparktrader.com/download
+make sure that the Spark app is running in the background
--- project.py
+++ project.py
@@ -1,4 +1,15 @@
-print("hello")
-
 import win32ui
 import dde
+
+serverName = "Spark"
+topic = "BHP.Quote"
+command = "Price"
+
+server = dde.CreateServer()
+server.Create("TestClient")
+conversation = dde.CreateConversation(server)
+
+conversation.ConnectTo(serverName, topic)
+conversation.Exec(command)
+
+requestedData = conversation.Request("some")
