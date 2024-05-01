05fb818ca3c70ae17eddb0c1207337e1de5e1f3d
Akshat Mishra <admiralakshat@gmail.com>
2024-04-30 19:08:36 -0700

    edited Readme, completed the boilerplate code to bypass Excel

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
+This code can only run on windows machine (becuse `win32ui` is available only on Windows)
+
 install `win32ui` using `pip install pywin32`, and import it using `import win32ui`
 
 install `Spark` from https://sparktrader.com/download
--- project.py
+++ project.py
@@ -3,13 +3,17 @@ import dde
 
 serverName = "Spark"
 topic = "BHP.Quote"
+#topic = "BHP.Depth"
+
 command = "Price"
+#command = "!BuyCount.BuyVolume.Buy.Sell.SellVolume.SellCount"
 
 server = dde.CreateServer()
 server.Create("TestClient")
 conversation = dde.CreateConversation(server)
 
 conversation.ConnectTo(serverName, topic)
-conversation.Exec(command)
+# conversation.Exec(command)
 
-requestedData = conversation.Request("some")
+requestedData = conversation.Request(command)
+print(requestedData)
