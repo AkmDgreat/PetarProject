5dc537233ca15160d5458d2514a85b06e3d34e9b
Akshat Mishra <admiralakshat@gmail.com>
2024-05-03 16:54:49 -0700

    tried creating the historic function

--- src/exampleScript.py
+++ src/exampleScript.py
@@ -1,7 +1,25 @@
 # use cases
 
 import connect
+import writeCsv
 
+# trying some thing
+#topic = "BHP.order"
+topic = "BHP.Depth"
+field = "Time.Date.Price"
+requestedData = connect.command(topic, field)
+print(requestedData)
+writeCsv.writeCsv("csv/historic.csv", requestedData)
+# print(requestedData)
+
+
+
+topic = "ASX.indexValues"
+field = "Time.Date.Price"
+requestedData = connect.command(topic, field)
+# print(requestedData)
+
+'''
 # Getting the current price of the stock BHP
 topic = "BHP.Quote"
 field = "Code.Price"
@@ -13,3 +31,4 @@ topic = "BHP.History"
 field = "Code.Price.10" 
 last10EODValuesOfBHP = connect.command(topic, field)
 print("Last 10 EOD price of BHP \n", last10EODValuesOfBHP)
+'''
