31f122f24a583af2c9ab92c42874d76b7cc0da76
AkmDgreat <admiralakshat@gmail.com>
2024-05-08 14:42:19 -0700

    completed the real time data function

--- src/CONSTANTS.py
+++ src/CONSTANTS.py
@@ -1,16 +1,6 @@
 from datetime import datetime
 
-# Start and end times of the market 
 START_TIME = datetime.now().replace(hour=6, minute=30, second=0, microsecond=0)
-END_TIME = datetime.now().replace(hour=13, minute=50, second=0, microsecond=0)
+END_TIME = datetime.now().replace(hour=13, minute=0, second=0, microsecond=0)
 TIME_INTERVAL_TO_WRITE_MARKET_DATA = 120
 TIME_INTERVAL_TO_WRITE_SIG_VALUE = 30
-
-
-# just for testing purposes
-# Start and end times are changed so that I can test the code right now, instead of waiting for time to be 6AM
-# time intervals are made shorter, to make testing faster
-START_TIME = datetime.now().replace(hour=18, minute=00, second=0, microsecond=0)
-END_TIME = datetime.now().replace(hour=22, minute=00, second=0, microsecond=0)
-TIME_INTERVAL_TO_WRITE_MARKET_DATA = 2
-TIME_INTERVAL_TO_WRITE_SIG_VALUE = 3
--- src/realTimeData.py
+++ src/realTimeData.py
@@ -1,30 +1,12 @@
 from writeMarketData import writeMarketData
 from writeSigValue import writeSigValue
-from runFunctionsConcurrently import runConcurrently
-import threading
-
-# def realTimeData(stockName):
-#     runConcurrently(writeSigValue(stockName), writeMarketData(stockName))
-
+import multiprocessing
 
 def realTimeData(stockName):
-    # if __name__ == "__main__":
-    #     runConcurrently(writeSigValue, writeMarketData, stockName)
     if __name__ == "__main__":
-        proc = threading.Thread(target = writeMarketData, args=(stockName))
-        proc.start()
-        proc2 = threading.Thread(target= writeSigValue, args=(stockName))
-        proc2.start()
-
-def realTimeData(stockName):
-    t1 = threading.Thread(target=writeMarketData, args=[stockName])
-    t2 = threading.Thread(target=writeSigValue, args=[stockName])
-    t1.start()
-    t2.start()
-
-    t1.join()
-    t2.join()
-
-#also try concurent.futures
-
-realTimeData("BHP")
+        t1 = multiprocessing.Process(target=writeMarketData, args=[stockName])
+        t2 = multiprocessing.Process(target=writeSigValue, args=[stockName])
+        t1.start()
+        t2.start()
+        t1.join()
+        t2.join()
